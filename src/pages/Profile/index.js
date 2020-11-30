7622c5137330f4668cf5b261148013bc49165c1e
SousaVictorH <victor.aquino@ccc.ufcg.edu.br>
2020-11-30 16:56:05 -0300

    finalizando eventos de keyup e deletando yarn.lock

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+
+function profile() {
+  return (
+    <h1>Profile</h1>
+  );
+}
+
+export default profile;
---
+++ src/pages/Search/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+
+function search() {
+  return (
+    <h1>Search</h1>
+  );
+}
+
+export default search;
